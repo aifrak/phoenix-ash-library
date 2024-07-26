5f6364e8a93ecc485a8f6ce10e8ad22fd3095636
aifrak <aifrak@users.noreply.github.com>
2024-07-26 10:03:45 +0000

    Remove warnings

--- library/lib/library/catalog/book/validations/IsISBN.ex
+++ library/lib/library/catalog/book/validations/IsISBN.ex
@@ -2,7 +2,7 @@ defmodule Library.Catalog.Book.Validations.IsISBN do
   use Ash.Resource.Validation
 
   @impl true
-  def validate(changeset, opts, _context) do
+  def validate(changeset, _opts, _context) do
     isbn = Ash.Changeset.get_attribute(changeset, :isbn)
 
     if is_nil(isbn) || Exisbn.valid?(isbn),
