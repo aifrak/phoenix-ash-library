b447e1ec732e957211c377463f153cd356256e2b
aifrak <aifrak@users.noreply.github.com>
2024-07-27 14:06:45 +0000

    Add comments

--- library/priv/repo/seeds.exs
+++ library/priv/repo/seeds.exs
@@ -22,27 +22,42 @@ Repo.delete_all(Feedback.Review)
 Repo.delete_all(Catalog.Author)
 Repo.delete_all(Catalog.Book)
 
-catalog_authors = CatalogFactory.insert!(Catalog.Author, count: 2)
+### Catalog ###
+
+# Books without authors
 CatalogFactory.insert!(Catalog.Book, count: 2)
+
+# 1 author per book
 CatalogFactory.insert!(Catalog.Book, count: 2, build: :authors)
 
+# Published books
 published_books =
   CatalogFactory.insert!(Catalog.Book, variant: :published, count: 2, build: :authors)
 
 published_book_1 = Enum.at(published_books, 0)
 published_book_2 = Enum.at(published_books, 1)
 
+# Book authors
+catalog_authors = CatalogFactory.insert!(Catalog.Author, count: 2)
+
+# Books with more than 1 author
 CatalogFactory.insert!(Catalog.Book, count: 2, relate: [authors: catalog_authors])
 
+# Published books with more than 1 author
 CatalogFactory.insert!(Catalog.Book,
   variant: :published,
   count: 2,
   relate: [authors: catalog_authors]
 )
 
+### Feedback ###
+
+# Authors
 feedback_authors = FeedbackFactory.insert!(Feedback.Author, count: 2)
 feedback_author_1 = Enum.at(feedback_authors, 0)
 feedback_author_2 = Enum.at(feedback_authors, 1)
+
+# Published book with 2 reviews and 1 comment
 
 feedback_review_1 =
   FeedbackFactory.insert!(Feedback.Review,
@@ -53,6 +68,13 @@ feedback_review_2 =
   FeedbackFactory.insert!(Feedback.Review,
     relate: [book: published_book_1, author: feedback_author_2]
   )
+
+FeedbackFactory.insert!(Feedback.Comment,
+  count: 2,
+  relate: [review: feedback_review_2, author: feedback_author_1]
+)
+
+# Published book with 1 review and 2 comment
 
 FeedbackFactory.insert!(Feedback.Review,
   relate: [book: published_book_2, author: feedback_author_2]
@@ -66,9 +88,4 @@ FeedbackFactory.insert!(Feedback.Comment,
 FeedbackFactory.insert!(Feedback.Comment,
   count: 2,
   relate: [review: feedback_review_1, author: feedback_author_2]
-)
-
-FeedbackFactory.insert!(Feedback.Comment,
-  count: 2,
-  relate: [review: feedback_review_2, author: feedback_author_1]
 )
