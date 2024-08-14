e5992aff3147cf76fc38dab673d7b1ec4615d0c0
aifrak <aifrak@users.noreply.github.com>
2024-08-14 18:50:02 +0000

    Remove warning from ash_postgres

--- library/lib/library/repo.ex
+++ library/lib/library/repo.ex
@@ -5,4 +5,9 @@ defmodule Library.Repo do
   def installed_extensions do
     ["ash-functions", "uuid-ossp", "citext", AshMoney.AshPostgresExtension]
   end
+
+  # Required by ash_postgres
+  def min_pg_version do
+    %Version{major: 16, minor: 0, patch: 0}
+  end
 end
