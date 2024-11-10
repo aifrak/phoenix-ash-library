09e5fb9672bc71f0a186a31e497b8d877724e194
aifrak <aifrak@users.noreply.github.com>
2024-11-10 09:54:47 +0100

    Do not use shims whe initializing mise

--- devbox/scripts/init_mise.sh
+++ devbox/scripts/init_mise.sh
@@ -8,7 +8,7 @@ MISE_ENV=${MISE_ENV:-local}
 
 # Add "--shims" when it is not interactive mode
 # see: https://mise.jdx.dev/cli/activate.html
-[[ $- == *i* ]] && shims_flag="--shims" || shims_flag=""
+# [[ $- == *i* ]] && shims_flag="--shims" || shims_flag=""
 
 # Initializes mise in the current shell session
 current_shell=$(basename "$SHELL")
