eea0a8a6929414ea198ac381450069305a4176c6
aifrak <aifrak@users.noreply.github.com>
2024-11-10 14:17:34 +0100

    Show task timings

--- scripts/mise/init.sh
+++ scripts/mise/init.sh
@@ -6,6 +6,7 @@ printf "%s\n" "-- Initializing mise --"
 mise settings set experimental true
 
 MISE_ENV=${MISE_ENV:-local}
+MISE_TASK_TIMINGS=1
 
 # Add "--shims" when it is not interactive mode
 # see: https://mise.jdx.dev/cli/activate.html
