6852d428bc7527be087c06bca9e1cc7e9de99123
aifrak <aifrak@users.noreply.github.com>
2025-10-03 14:25:13 +0200

    Bump phoenix_live_reload to 1.6.1

--- library/mix.exs
+++ library/mix.exs
@@ -75,7 +75,7 @@ defmodule Library.MixProject do
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_reload, "~> 1.6.1", only: :dev},
       {:phoenix_live_view, "~> 1.1.13", override: true},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
